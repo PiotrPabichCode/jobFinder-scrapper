2082c383008722b9d87caf4884b7ed9887e94d75
PiotrPabich <87279370+PiotrPabichCode@users.noreply.github.com>
2023-12-09 22:03:52 +0100

    Version 1.02 - Changed pipelines

--- nofluffjobs_scraper/pipelines.py
+++ nofluffjobs_scraper/pipelines.py
@@ -5,6 +5,8 @@
 
 
 # useful for handling different item types with a single interface
+import re
+
 from itemadapter import ItemAdapter
 
 class NofluffjobsScraperPipeline:
@@ -12,22 +14,46 @@ class NofluffjobsScraperPipeline:
 
         adapter = ItemAdapter(item)
 
-        # Stripping whitespaces
-        field_names = adapter.field_names()
-        without_stripping = ['position_description', 'requirements_description']
-        for field_name in field_names:
+        # Nested Fields
+        nested_fields = ["salaries", "skills", "requirements_description", "offer_description", "responsibilities", "job_details", "methodology", "benefits", "company_details"]
+        for field_name in nested_fields:
             value = adapter.get(field_name)
-            if field_name not in without_stripping:
+            adapter[field_name] = value[0] if value and len(value) > 0 else []
 
-                if value is not None and len(value) > 0:
-                    first_value = value[0]
-                    if isinstance(item, str):
-                        adapter[field_name] = first_value.strip()
-                    elif isinstance(first_value, dict):
-                        adapter[field_name] = {k: v.strip() for k, v in first_value.items()}
+        # Single String Fields
+        single_fields = ["url", "image", "position_name", "position_seniority", "company_name", "company_description", "requirements_description", "offer_description", "responsibilities"]
+        for field_name in single_fields:
+            value = adapter.get(field_name)
+            if value and len(value) > 0 and value[0]:
+                adapter[field_name] = value[0].strip()
+            else:
+                adapter[field_name] = ""
 
-        # salaries
-        salaries = adapter.get('salaries')[0]  # Extracting the list from the tuple
+        # Strip Array Fields
+        strip_array_fields = ["skills", "job_details", "methodology", "benefits", "company_details"]
+        for field_name in strip_array_fields:
+            values = adapter.get(field_name)
+            adapter[field_name] = [detail.strip() for detail in values]
+
+        # Extract Location
+        company_details = adapter.get('company_details')
+        if len(company_details) > 2:
+            location = company_details[2]
+            pattern = r'(?:Locations?|Main location):\s*([\w\s\d+]+)'
+            match = re.search(pattern, location)
+            if match:
+                adapter['company_details'][2] = match.group(1).strip()
+
+        # Clear NBSP
+        clear_nbsp = ["requirements_description", "offer_description"]
+        for field_name in clear_nbsp:
+            value = adapter.get(field_name)
+            adapter[field_name] = value.replace(u"\xa0", " ")
+        if len(company_details) > 3:
+            adapter['company_details'][3] = company_details[3].replace(u"\xa0", " ")
+
+        # Salaries
+        salaries = adapter.get('salaries')
         for salary in salaries:
             employment_type = salary.get('employment_type')
 
